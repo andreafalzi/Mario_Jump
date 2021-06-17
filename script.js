98d05d941906d7633e39cbb4f72f8e2b39c17dfe
Andrea Falzi <andrea.falzi1988@gmail.com>
2021-06-17 21:02:41 +0200

    responsive

--- script.js
+++ script.js
@@ -16,25 +16,11 @@ function moveMario(){
 }
 function jumpMario(){
     var elem = document.getElementById("mario");
-    var pos = 6;
+    var pos = 16;
     clearInterval(id);
     id = setInterval(frame, 30);
     function frame(){
-        if (pos == 21) {
-            clearInterval(id);
-        } else {
-            pos++;
-            elem.style.bottom = pos + '%';
-        }
-    }
-}
-function jumpMarioIpad(){
-    var elem = document.getElementById("mario");
-    var pos = 61;
-    clearInterval(id);
-    id = setInterval(frame, 50);
-    function frame(){
-        if (pos == 67) {
+        if (pos == 32) {
             clearInterval(id);
         } else {
             pos++;
@@ -46,19 +32,12 @@ function jumpMarioIpad(){
 function resetMario(){
     var elem = document.getElementById("mario");
     var x = document.getElementById("burger");
-    elem.style.bottom = 6 + '%';
+    elem.style.bottom = 16 + '%';
     elem.style.left = 10 + '%';
     x.style.opacity = 0;
     x.style.transform = "scaleY(0)";
 }
-function resetMarioIpad(){
-    var elem = document.getElementById("mario");
-    var x = document.getElementById("burger");
-    elem.style.bottom = 61 + '%';
-    elem.style.left = 10 + '%';
-    x.style.opacity = 0;
-    x.style.transform = "scaleY(0)";
-}
+
 
 function burgerShow(){
     var elem = document.getElementById("burger");
@@ -79,10 +58,5 @@ function burgerShow(){
 function actionMario() {
     setTimeout(moveMario, 900);
     setTimeout(jumpMario, 3000);
-    burgerShow();
-}
-function actionMarioIpad() {
-    setTimeout(moveMario, 900);
-    setTimeout(jumpMarioIpad, 3000);
     burgerShow();
 }
